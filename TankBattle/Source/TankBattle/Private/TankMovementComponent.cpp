080d816c4f210147e614c95829a16336a803e7bf
norlin <myxaxaxa@gmail.com>
2017-03-02 09:30:52 +0300

    fix pointers check

--- TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
+++ TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
@@ -5,20 +5,24 @@
 #include "TankMovementComponent.h"
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
-	if (!LeftTrackToSet || !RightTrackToSet) {
-		return;
-	}
-
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw) {
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
